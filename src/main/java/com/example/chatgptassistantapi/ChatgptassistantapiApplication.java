21c046d6389d990893f02ebe8d72a823bdafa0d8
Imesh-Isuranga <imeshisuranga00@gmail.com>
2023-10-20 02:46:46 +0530

    first commit

---
+++ src/main/java/com/example/chatgptassistantapi/ChatgptassistantapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptassistantapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptassistantapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptassistantapiApplication.class, args);
+    }
+
+}
