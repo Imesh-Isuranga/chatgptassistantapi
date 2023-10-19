1678c3a59ab417f41561367fc267c9c0033df2af
Imesh-Isuranga <imeshisuranga00@gmail.com>
2023-10-20 04:15:27 +0530

    Created controllers and models

---
+++ src/main/java/com/example/chatgptassistantapi/model/Choice.java
@@ -0,0 +1,16 @@
+package com.example.chatgptassistantapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Choice {
+
+    private int index;
+    private Message message;
+}
